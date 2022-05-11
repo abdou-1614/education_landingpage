e322395baf06fda881d4ca2401f62d16f2ea7b52
abdou-1614 <kirakira1614@gmail.com>
2022-05-11 08:19:53 -0700

    five

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import course1 from './images/course-1.jpg'
-import {FaRegStar} from 'react-icons/fa'
+import {RiStarFill} from 'react-icons/ri'
 import student from './images/student-icon.png'
 import course2 from './images/course-2.jpg'
 import course3 from './images/course-3.jpg'
@@ -26,19 +26,19 @@ const Course = () => {
             <h3>
               <a href='#'>Become product manager learn skills.</a>
             </h3>
-            <div className='wrraperB'>
+            <div className='wrapper bo'>
               <div className='author'>
                   <img src={courseI} alt="course
                   Instructor"/>
                   <a href='#'>Lillian Wals</a>
               </div>
               <div className='rating'>
-                <FaRegStar/>
+                <RiStarFill className="icon"/>
                 <p>5.0 (2k)</p>
               </div>
             </div>
-              <div className='wrraper'>
-                  <div>$50.00</div>
+              <div className='wrapper'>
+                  <div className='price'>$50.00</div>
                   <div className='enroll'>
                       <div className='icon-user'>
                           <img src={student} alt="student"/>
@@ -61,19 +61,19 @@ const Course = () => {
             <h3>
               <a href='#'>Fashion and luxury fashion in a changing.</a>
             </h3>
-            <div className='wrraperB'>
+            <div className='wrapper bo'>
               <div className='author'>
                   <img src={courseI} alt="course
                   Instructor"/>
                   <a href='#'>Lillian Wals</a>
               </div>
               <div className='rating'>
-                <FaRegStar/>
+                <RiStarFill className="icon"/>
                 <p>4.7 (5k)</p>
               </div>
             </div>
-              <div className='wrraper'>
-                  <div>$80.00</div>
+              <div className='wrapper'>
+                  <div className='price'>$80.00</div>
                   <div className='enroll'>
                       <div className='icon-user'>
                           <img src={student} alt="student"/>
@@ -96,19 +96,19 @@ const Course = () => {
             <h3>
               <a href='#'>Learning to write as a professional.</a>
             </h3>
-            <div className='wrraperB'>
+            <div className='wrapper bo'>
               <div className='author'>
                   <img src={courseI} alt="course
                   Instructor"/>
                   <a href='#'>Lillian Wals</a>
               </div>
               <div className='rating'>
-                <FaRegStar/>
+                <RiStarFill className="icon"/>
                 <p>4.1 (3k)</p>
               </div>
             </div>
-              <div className='wrraper'>
-                  <div>$29.90</div>
+              <div className='wrapper'>
+                  <div className='price'>$29.90</div>
                   <div className='enroll'>
                       <div className='icon-user'>
                           <img src={student} alt="student"/>
@@ -131,19 +131,19 @@ const Course = () => {
             <h3>
               <a href='#'>Improving accessibility of Your markdown.</a>
             </h3>
-            <div className='wrraperB'>
+            <div className='wrapper bo'>
               <div className='author'>
                   <img src={courseI} alt="course
                   Instructor"/>
                   <a href='#'>Lillian Wals</a>
               </div>
               <div className='rating'>
-                <FaRegStar/>
+                <RiStarFill className="icon"/>
                 <p>4.8 (3.9k)</p>
               </div>
             </div>
-              <div className='wrraper'>
-                  <div>$49.90</div>
+              <div className='wrapper'>
+                  <div className='price'>$49.90</div>
                   <div className='enroll'>
                       <div className='icon-user'>
                           <img src={student} alt="student"/>
@@ -166,19 +166,19 @@ const Course = () => {
             <h3>
               <a href='#'>Master query in a short period of time.</a>
             </h3>
-            <div className='wrraperB'>
+            <div className='wrapper bo'>
               <div className='author'>
                   <img src={courseI} alt="course
                   Instructor"/>
                   <a href='#'>Lillian Wals</a>
               </div>
               <div className='rating'>
-                <FaRegStar/>
+                <RiStarFill className="icon"/>
                 <p>3.8 (1k)</p>
               </div>
             </div>
-              <div className='wrraper'>
-                  <div>$89.00</div>
+              <div className='wrapper'>
+                  <div className='price'>$89.00</div>
                   <div className='enroll'>
                       <div className='icon-user'>
                           <img src={student} alt="student"/>
@@ -201,19 +201,19 @@ const Course = () => {
             <h3>
               <a href='#'>Business Intelligence analyst Course 2022.</a>
             </h3>
-            <div className='wrraperB'>
+            <div className='wrapper bo'>
               <div className='author'>
                   <img src={courseI} alt="course
                   Instructor"/>
                   <a href='#'>Lillian Wals</a>
               </div>
               <div className='rating'>
-                <FaRegStar/>
+                <RiStarFill className="icon"/>
                 <p>4.9 (23k)</p>
               </div>
             </div>
-              <div className='wrraper'>
-                  <div>$199.00</div>
+              <div className='wrapper'>
+                  <div className='price'>$199.00</div>
                   <div className='enroll'>
                       <div className='icon-user'>
                           <img src={student} alt="student"/>
